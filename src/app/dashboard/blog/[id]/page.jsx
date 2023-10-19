caf209469c2adab07083b91dd8f259afc869025b
Al Hassan <mateenmahihot@gmail.com>
2023-10-19 23:23:22 +0500

    50% responsive

--- src/app/dashboard/blog/[id]/page.jsx
+++ src/app/dashboard/blog/[id]/page.jsx
@@ -19,72 +19,61 @@ async function getData(id) {
 }
 
 const BlogPost = async ({ params }) => {
-    const res = await getData(params.id);
-    const postData = res;
+  const res = await getData(params.id);
+  const postData = res;
 
-    return (
-      <div className={styles.container}>
-        <div className={styles.custumiseContainer}>
-          <Link
-            href={`/dashboard/blog/${params.id}/edit/${params.id}`}
-            className={styles.linkE}
-          >
-            <FaEdit /> Edit
-          </Link>
-          <Link
-            href={`/dashboard/blog/${params.id}/delete/${params.id}`}
-            className={styles.linkD}
-          >
-            <FaTrash /> Delete
-          </Link>
-        </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.customizeContainer}>
+        <Link href={`/dashboard/blog/${params.id}/edit/${params.id}`} className={styles.linkE}>
+          <FaEdit /> Edit
+        </Link>
+        <Link href={`/dashboard/blog/${params.id}/delete/${params.id}`} className={styles.linkD}>
+          <FaTrash /> Delete
+        </Link>
+      </div>
 
-        <div className={styles.firstContainer}>
-          <div className={styles.textContainer}>
-            <h3 className={styles.heading}>{postData.title}</h3>
-            <div className={styles.AuthorInfo}>
-              <Image
-                src={postData.authorImg}
-                alt={postData.author}
-                className="small-rounded-img"
-                width={50}
-                height={50}
-              />
-              <h5 className={styles.AuthorName}>{postData.author}</h5>
-            </div>
-          </div>
-          <div className={styles.imageContainer}>
+      <div className={styles.firstContainer}>
+        <div className={styles.textContainer}>
+          <h3 className={styles.heading}>{postData.title}</h3>
+          <div className={styles.AuthorInfo}>
             <Image
-              src={postData.img}
-              alt={postData.img}
-              layout="fill"
-              objectFit="cover"
+              src={postData.authorImg}
+              alt={postData.author}
+              className={styles.smallRoundedImg}
+              width={50}
+              height={50}
             />
+            <h5 className={styles.AuthorName}>{postData.author}</h5>
           </div>
         </div>
-        <div className={styles.container}>
-          <div className={styles.subContainer3}>
-            <div className={styles.Blog}>
-            <BlogContent content = {postData.content} />
-              <Comment />
-            </div>
-            <div className={styles.Sidemenu}>
-              <Sidemenu />
-            </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src={postData.img || "/no.jpg"}
+            alt={postData.img}
+            layout="responsive" // Use responsive layout
+            width={800} // Set a fixed width or use a percentage
+            height={400} // Set a fixed height or use a percentage
+          />
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.subContainer3}>
+          <div className={styles.Blog}>
+            <BlogContent content={postData.content} />
+            <Comment />
           </div>
         </div>
       </div>
-   
-    );
-  
+    </div>
+  );
 };
 
 export default BlogPost;
 
 export function generateMetadata() {
- 
   return {
-    title:  "Irfan Sir | Blog Post",
+    title: "Irfan Sir | Blog Post",
     description: "Xeven Solutions",
   };
 }
